b5b192431f87f2c3267ffe9a797020ade7a02f13
sarah-zhan <yizh.contact@gmail.com>
2023-11-27 14:01:09 -0800

    built the Dockerfile

--- week9/lambda_function.py
+++ week9/lambda_function.py
@@ -2,28 +2,9 @@ import tflite_runtime.interpreter as tflite
 from keras_image_helper import create_preprocessor
 
 
-url = "https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg"
+# url = "https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg"
 
 classes = ["bee", "wasp"]
-
-# def download_image(url):
-#     with request.urlopen(url) as resp:
-#         buffer = resp.read()
-#     stream = BytesIO(buffer)
-#     img = Image.open(stream)
-#     return img
-
-
-# def prepare_image(img, target_size):
-#     if img.mode != 'RGB':
-#         img = img.convert('RGB')
-#     img = img.resize(target_size, Image.NEAREST)
-#     return img
-
-# def preprocess_input(x):
-#     x /= 127.5 #This line scales the data from the range [0,255] to [0,2].
-#     x -= 1. #This line shifts the data from the range [0,2] to [-1,1].
-#     return x
 
 preprocess = create_preprocessor("xception", target_size=(150, 150))
 
@@ -34,7 +15,7 @@ input_index = interpreter.get_input_details()[0]["index"]
 output_index = interpreter.get_output_details()[0]["index"]
 
 
-def predict():
+def predict(url):
 
     X = preprocess.from_url(url)
 
