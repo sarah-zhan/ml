0b7f6296d5a373134ce2b5a338eb33cf9dcba491
sarah-zhan <yizh.contact@gmail.com>
2023-11-27 23:49:02 -0800

    add intro

--- week9/test.py
+++ week9/test.py
@@ -1,9 +1,10 @@
 import requests
 
-url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+# url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+url = "https://qxqypeg9z3.execute-api.us-east-2.amazonaws.com/test/predict"
 
 data = {
-    'url': 'https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg'
+    "url": "https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg"
 }
 
 result = requests.post(url, json=data).json()
