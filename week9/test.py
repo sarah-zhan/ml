f5455c8af4d5d8342eee1846ea429a5ba862b73d
sarah-zhan <yizh.contact@gmail.com>
2023-11-29 23:10:00 -0800

    make api gateway work

--- week9/test.py
+++ week9/test.py
@@ -1,11 +1,13 @@
-import requests
+import boto3
 
-# url = "http://localhost:8080/2015-03-31/functions/function/invocations"
-url = "https://qxqypeg9z3.execute-api.us-east-2.amazonaws.com/test/predict"
+client = boto3.client('apigateway')
+response = client.test_invoke_method(
+    restApiId='qxqypeg9z3',
+    resourceId='3jq5dco7bi',
+    httpMethod='POST',
+    pathWithQueryString='/test/predict',
+    body='{"url": "https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg"}'
+)
+print(response['body'])
 
-data = {
-    "url": "https://habrastorage.org/webt/rt/d9/dh/rtd9dhsmhwrdezeldzoqgijdg8a.jpeg"
-}
 
-result = requests.post(url, json=data).json()
-print(result)
